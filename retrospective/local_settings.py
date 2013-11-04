c641b7feb700e7fd4bfe361e8f1dfc788b818bba
Justyna Ilczuk <att@atte.ro>
2013-11-04 21:35:22 +0100

    Remove localsettings

--- retrospective/local_settings.py
+++
@@ -1,9 +0,0 @@
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devel-retr',                      # Or path to database file if using sqlite3.
-        'USER': 'devel-retr',
-        'PASSWORD': 'none',
-    }
-}
