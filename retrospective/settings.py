574c232b15ca9cf80e74e8d79147cf026d8626e3
Justyna Ilczuk <att@atte.ro>
2013-11-04 21:44:52 +0100

    change settings

--- retrospective/settings.py
+++ retrospective/settings.py
@@ -8,13 +8,14 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-db_pass = os.environ.get('DB-PASS', 'devel-retr')
+db_pass = os.environ.get('DB-PASS', 'devel_retr')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devel-retr',                      # Or path to database file if using sqlite3.
-        'USER': 'devel-retr',
+        'NAME': 'devel_retr',                      # Or path to database file if using sqlite3.
+        'USER': 'devel_retr',
         'PASSWORD': db_pass,
+        'HOST': ''
     }
 }
 
